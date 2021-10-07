d922d387fbd5740dd0100b5e0504278ee61fe699
Neffpr <92007267+Neffpr@users.noreply.github.com>
2021-10-06 20:44:16 -0500

    Update Main.java
     successfully updated someone else's project.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -3,7 +3,7 @@ package Main;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello, my name is Sean Mc Hugh.");
+	System.out.println("Hello, my name is Sean Mc Hugh.");
         System.out.println("Hello Sean, my name is Nefi Perez.");
     }
 }
